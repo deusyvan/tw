6cc7383c0356205f47e043716f4f9bb9e297eb20
Deusyvan Silva <deusyvan@dfsweb.com.br>
2019-12-09 23:44:54 -0300

    Continauando a configuração

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php 
+session_start();
+require 'twconfig.php';
+require 'twitteroauth/autoload.php';
+use Abraham\TwitterOAuth\TwitterOAuth;
+
+if(isset($_SESSION)){
+    
+}
+?>
---
+++ twconfig.php
@@ -0,0 +1,5 @@
+<?php 
+define("CONSUMER_KEY", 'sua_key');
+define("CONSUMER_SECRET", 'sua_secret');
+define("OAUTH_CALLBACK", 'http://127.0.0.1/tw/callback.php');
+?>
